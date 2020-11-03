15fc99ac40151a4aac71d6d2611610701eddcb9a
AkatSuKi_SuKa <vankiet.kn78@gmail.com>
2020-11-03 22:46:46 +0700

    npm i react --save

--- src/Page/Contacts/Contacts.js
+++ src/Page/Contacts/Contacts.js
@@ -3,7 +3,7 @@ import Footer from '../../component/Footer/index'
 import Header from '../../component/Header';
 import GoogleMap from '../../component/ContactGgMap';
 import AnimationFigma from '../../component/AnimationFigma/index'
-import Contact_TellUs from '../../component/Contact_TellUs';
+import ContactTellUs from '../../component/Contact_TellUs';
 
 
 
@@ -16,7 +16,7 @@ class Contact extends React.Component {
         <Header />
         <AnimationFigma />
         <GoogleMap />
-        <Contact_TellUs />
+        <ContactTellUs />
         {<Footer />}
       </div>
     );
